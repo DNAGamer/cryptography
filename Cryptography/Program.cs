77b4f73bd652c9fe58ab2b667934a587964603e9
DNA <DNAGamer@users.noreply.github.com>
2017-10-25 09:48:29 +0100

    Added shit
    Added clause to stop the code "decrypting" punctuation, and added code to brute force the stretch task, because im kinky like that

--- Cryptography/Program.cs
+++ Cryptography/Program.cs
@@ -11,25 +11,32 @@ namespace Cryptography
     {
         static void Main(string[] args)
         {
-            Console.Write("Please select a mode:\n\n1)Encrypt\n2)Decrypt\n");
+            Console.Write("Please select a mode:\n\n1)Encrypt\n2)Decrypt\n3)Stretch Task\n");
             int mode = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter your text: ");
-            string input = Console.ReadLine();
-            Console.Write("What is the character shift? ");
-            int shift = Convert.ToInt32(Console.ReadLine());
-
-            if (mode == 1)
+            if (mode != 3)
             {
-                string encrypted = Encrypt(input, shift);
-                Console.WriteLine(encrypted);
+                Console.Write("Enter your text: ");
+                string input = Console.ReadLine().Replace("\n", "");
+                Console.Write("What is the character shift? ");
+                int shift = Convert.ToInt32(Console.ReadLine());
+            
+                if (mode == 1)
+                {
+                    string encrypted = Encrypt(input, shift);
+                    Console.WriteLine(encrypted);
+                }
+                else if (mode == 2)
+                {
+                    string decrypted = Decrypt(input, shift);
+                    Console.WriteLine(decrypted);
+                }
+                Console.ReadKey();
             }
-            else if (mode == 2)
+            else
             {
-                string decrypted = Decrypt(input, shift);
-                Console.WriteLine(decrypted);
+                Stretch();
             }
 
-            Console.ReadKey();
         }
         
         static string Encrypt(string text, int shift)
@@ -60,18 +67,43 @@ namespace Cryptography
             for(int i =0; i < characters.Length; i++)
             {
                 char letter = characters[i]; //Get our letter into a variable to make things easier
-                letter = (char)(letter - shift); //Shift our letters back the desired amount
-                if (letter > 'z') //The upper limit is 'z' (because we only have 26 letters), so subtract 26 (the alphabet) if we overflow the max
-                {
-                    letter = (char)(letter - 26);
+                if (char.IsLetter(letter))
+                    {
+                    letter = (char)(letter - shift); //Shift our letters back the desired amount
+                    if (letter > 'z') //The upper limit is 'z' (because we only have 26 letters), so subtract 26 (the alphabet) if we overflow the max
+                    {
+                        letter = (char)(letter - 26);
+                    }
+                    else if (letter < 'a') //The lower limit is 'a' (because a is the lowest letter), so add 26 if we underflow the array
+                    {
+                        letter = (char)(letter + 26);
+                    }
+                    characters[i] = letter; //replace the origional letter with the 'encrypted' letter
                 }
-                else if (letter < 'a') //The lower limit is 'a' (because a is the lowest letter), so add 26 if we underflow the array
-                {
-                    letter = (char)(letter + 26);
-                }
-                characters[i] = letter; //replace the origional letter with the 'encrypted' letter
             }
             return new string(characters);
+        }
+
+        static void Stretch()
+        {
+            string text = @"Mu husudjbo iqm, veh jxu vyhij jycu, jxu huikbj ev q iefxyijysqjut hqdiecmqhu qjjqsa, qdt
+jxu uvvusji jxqj yj xqt ed jxu Dqjyedqb Xuqbjx Iuhlysu, ydsbktydw qcrkbqdsui ruydw jkhdut
+qmqo qdt fqjyudji xqlydw jxuyh efuhqjyedi sqdsubbut mxybij fhufqhydw je we yd je
+jxuqjhu.Jxu MqddqSho lyhki, qi yj rusqcu ademd, xqt vekdt q auo lkbduhqrybyjo, qdt yj
+xqt q tulqijqjydw ycfqsj qi edu xeifyjqb qvjuh qdejxuh hufehjut jxqj yj xqt ruud qvvusjut;
+duqhbo vyvjo yd jejqb. Yd jxu yccutyqju qvjuhcqjx, unfuhji qdt ydiytuhi qbyau mudj ed je
+dumi ekjbuji qdt ed je iesyqb cutyq, myjx jxuyh unfbqdqjyedi eh xem jxyi sekbt xqlu
+xqffudut.Mxqj yi sbuqh yi jxqj iecujxydw duutut je ru tedu je udikhu jxqj iksx qd qjjqsa
+mybb duluh xqlu jxu iqcu ycfqsj ed jxu Dqjyedqb Xuqb Iuhlysu yd jxu vkjkhu.";
+            for (int i = 0; i < 26; i++)
+            {
+                string Decrypted = Decrypt(text, i);
+                if (Decrypted.Contains("he") && Decrypted.Contains("the"))
+                {
+                    Console.Write($"Shift {i}\n\nSolution:\n{Decrypted}");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
